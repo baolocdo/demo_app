bccaac8f30c67cda5245c0f4b68082606bbb1584
Loc Do <locdo@rocketmail.com>
2012-12-28 18:32:05 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '5e9f5aae46b7f076e69f1e55bb074b35469140df5335931ef30d36311048687571e5a091b227fe3c88e81e826c8263165ce9cde1278d84afb8e7dba3b189eddc'
